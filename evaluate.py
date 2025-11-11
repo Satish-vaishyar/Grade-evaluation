6ebfb9713a97f7d4d055f91b48fdef79c5eac40f
Satish H Vaishyar <12vsatish1@gmail.com>
2025-11-11 08:35:46 +0530

    Refactor marks input handling in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -14,16 +14,22 @@ def evaluate_grade(grade):
     
 if len(sys.argv) != 6:
     script_name = sys.argv[0]
-    marks1 = int(sys.argv[1])
-    marks2 = int(sys.argv[2])
-    marks3 = int(sys.argv[3])
-    marks4 = int(sys.argv[4])
-    marks5 = int(sys.argv[5])
+    marks1 = sys.argv[1]
+    marks2 = sys.argv[2]
+    marks3 = sys.argv[3]
+    marks4 = sys.argv[4]
+    marks5 = sys.argv[5]
+else:
+    marks1 = 90
+    marks2 = 85
+    marks3 = 78
+    marks4 = 92
+    marks5 = 88
 
-    total_marks = marks1 + marks2 + marks3 + marks4 + marks5
-    average_marks = total_marks / 5
-    grade = evaluate_grade(average_marks)
+total_marks = marks1 + marks2 + marks3 + marks4 + marks5
+average_marks = total_marks / 5
+grade = evaluate_grade(average_marks)
 
-    print(f"Total Marks: {total_marks}")
-    print(f"Average Marks: {average_marks}")
-    print(f"Grade: {grade}")
+print(f"Total Marks: {total_marks}")
+print(f"Average Marks: {average_marks}")
+print(f"Grade: {grade}")
